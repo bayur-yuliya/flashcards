05081a48abe8c62e1c853900c37e24ed5e40aa1d
Baiur Yuliia <juliy14497@gmail.com>
2026-07-30 21:16:07 +0300

    test pyside6

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,21 +1,36 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QLabel, QPushButton
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.count = 0
         self.initializeUI()
 
     def initializeUI(self):
+        self.setGeometry(200, 100, 500, 400)
         self.setWindowTitle("Flashcards")
-        self.setGeometry(100, 100, 800, 600)
 
-        self.create_menu()
+        self.setUpMenu()
+        self.setUpCounter()
         self.statusBar().showMessage("Message")
 
-    def create_menu(self):
+    def setUpMenu(self):
         menubar = self.menuBar()
         file_menu = menubar.addMenu("Menu")
 
         exit_action = file_menu.addAction("Exit")
         exit_action.triggered.connect(self.close)
+
+    def setUpCounter(self):
+        self.label = QLabel("Counter", self)
+        self.label.move(100, 100)
+        self.label_counter = QLabel(str(self.count), self)
+        self.label_counter.move(100, 150)
+        button = QPushButton("Increase", self)
+        button.move(100, 200)
+        button.clicked.connect(self.increaseCounter)
+
+    def increaseCounter(self):
+        self.count += 1
+        self.label_counter.setText(str(self.count))
